b743e2cb17ae8bcda161a36c62b2ad6bbf4f1cf1
Ramazan Ocak <rmznocak@gmail.com>
2022-07-25 22:28:00 +0300

    coin model class created and geturl method updated. some comments added

--- CryptoViewer/CryptoViewer/ViewController.swift
+++ CryptoViewer/CryptoViewer/ViewController.swift
@@ -8,10 +8,9 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else { return }
-            print(text)
-    }
+    
+    var coins = [Coin]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,19 +20,45 @@ class ViewController: UICollectionViewController, UISearchResultsUpdating {
         search.searchBar.placeholder = "Type something here to search"
         navigationItem.searchController = search
         title = "Crypto List"
+        
+        //Alt satırda api'den aldığımız cevabı oluşturduğumuz metoda verdik. Bundan sonraki tüm işlemlerimizi self.coins üzerinden yapacağız.
+        getUrl(completion: {response, err in
+            self.coins = response
+            
+            //Yukarda collectionview objesi oluşturup reload etmen gerek o yüzden aşağıyı şimdilik yorum olarak bırakıyorum.
+           // DispatchQueue.main.async {
+           //     self.collectionView.reloadData()
+           // }
+        })
     }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+            print(text)
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CryptoCollectionViewCell else {
             fatalError("CryptoCollectionViewCell not found")
         }
         return cell
     }
-    func getUrl() {
+    
+    func getUrl(completion: @escaping ([Coin], Error?) -> ()) {
         let url = URL(string: "https://api.coinstats.app/public/v1/coins")!
 
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
             guard let data = data else { return }
             print(String(data: data, encoding: .utf8)!)
+            do {
+                let coinResult = try JSONDecoder().decode(CoinModel.self, from: data)
+                completion(coinResult.coins, nil)
+                
+            } catch let jsonError {
+                print("Failed to decode json", jsonError)
+                completion([], jsonError)
+            }
+
         }
         task.resume()
     }
