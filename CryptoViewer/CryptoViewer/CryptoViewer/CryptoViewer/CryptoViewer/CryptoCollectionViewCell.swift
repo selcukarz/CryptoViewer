3771dbb1c6fe4271615616e91d0e547336658c38
selcukarz <107930904+selcukarz@users.noreply.github.com>
2022-07-25 15:13:50 +0300

    Model added

---
+++ CryptoViewer/CryptoViewer/CryptoViewer/CryptoViewer/CryptoViewer/CryptoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CryptoCollectionViewCell.swift
+//  CryptoViewer
+//
+//  Created by muzaffer on 19.07.2022.
+//
+
+import UIKit
+
+class CryptoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var idLabel : UILabel!
+    @IBOutlet weak var symbolLabel : UILabel!
+    @IBOutlet weak var priceLabel : UILabel!
+    @IBOutlet weak var priceChange1DLabel : UILabel!
+}
+
+
